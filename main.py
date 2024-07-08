6a9f6bafd7690e0d97bcfe8c9b6bd51f12954081
Raresstan0406 <stanrares02@yahoo.com>
2024-07-08 14:52:40 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-
+import datetime
 #1
 def caractere_interzise(c_i, date):
     for cuvant in date:
@@ -17,6 +17,7 @@ def validare_nume(nume):
 def validare_prenume(date):
     for cuvant in date:
         for c in cuvant:
+            if c.isdigit():
                 return 1
     return 0
 #4
@@ -35,11 +36,14 @@ def validare_ultima_cifra_cnp(verif, cnp):
     for nr in range(12):
         alg.append(int(cnp[nr]) * int(verif[nr]))
     suma = sum(alg)
-    if suma % 11 != int(cnp[12]):
+    cifra_control = suma % 11
+    if cifra_control == 10:
+        cifra_control = 1
+    if cifra_control != int(cnp[12]):
         return 1
     return 0
 
-def validare(dict, date):
+def validare(index, dict, date):
     ok = 0
     if caractere_interzise('+@!?\/', date) == 1:
         print("Datele contin caractere interzise")
@@ -61,16 +65,17 @@ def validare(dict, date):
         ok = 1
     if ok == 0:
         if len(date) == 3:
-            dict.update({"Nume ": date[0],
+            dict.update({"ID ": index,
+                         "Nume ": date[0],
                          "Prenume 1 ": date[1],
                          "Prenume 2": "-",
                          "CNP ": date[2]})
         elif len(date) == 4:
-            dict.update({"Nume": date[0],
+            dict.update({"ID ": index,
+                         "Nume": date[0],
                          "Prenume 1": date[1],
                          "Prenume 2": date[2],
                          "CNP": date[3]})
-
 def impartire_prenume(prenume, p):
     if '-' in prenume:
         impartire = prenume.split('-')
@@ -89,18 +94,51 @@ def impartire_date(cursant, date):
         for cuv in date_prime:
             date.append(cuv)
 
-dict = {}
-date = []
-cursant = input("Introduceti date cursant:")
-impartire_date(cursant, date)
-for i in range(len(date)-1):
-    if date[i][0]!= date[i][0].upper():
-        date[i]= date[i].capitalize()
-validare(dict, date)
-nume_col=dict.keys()
-if dict:
-    with open('data.csv', 'a', newline='') as file:
-        w = csv.DictWriter(file, fieldnames=nume_col)
-        if file.tell() == 0:
-            w.writeheader()
-        w.writerow(dict)
+
+lista = []
+index=1
+while True:
+    dict = {}
+    date = []
+    cursant = input("Introduceti date cursanti sau una din comenzile EXIT/SAVE:")
+    if cursant == "EXIT":
+        break
+    elif cursant == "SAVE":
+        decizie = input("Tip fisier: TXT/CSV: ")
+        timestamp = datetime.datetime.now()
+        if decizie == "CSV":
+            for dictionar in lista:
+                nume_col = dictionar.keys()
+                if dictionar:
+                    with open(f'Lista_cursanti_{timestamp.date().year}_{timestamp.date().month}_{timestamp.date().day}'
+                              f'_{timestamp.time().hour}_{timestamp.time().minute}.csv', 'a', newline='') as file:
+                        w = csv.DictWriter(file, fieldnames=nume_col)
+                        if file.tell() == 0:
+                            w.writeheader()
+                        w.writerow(dictionar)
+            lista = []
+            index = 1
+        elif decizie == "TXT":
+            for dictionar in lista:
+                nume_col = dictionar.keys()
+                if dictionar:
+                    with open(f'Lista_cursanti_{timestamp.date().year}_{timestamp.date().month}_{timestamp.date().day}'
+                              f'_{timestamp.time().hour}_{timestamp.time().minute}.txt', 'a', newline='') as file:
+                        if file.tell() == 0:
+                            header = ' | '.join(nume_col) + '\n'
+                            file.write(header)
+                        row = ' | '.join(str(dictionar[field]) for field in nume_col) + '\n'
+                        file.write(row)
+            lista = []
+            index = 1
+    else:
+        impartire_date(cursant, date)
+        for i in range(len(date)-1):
+            if date[i][0] != date[i][0].upper():
+                date[i] = date[i].capitalize()
+        validare(index, dict, date)
+        index += 1
+        lista.append(dict)
+
+
+
